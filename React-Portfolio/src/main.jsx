46c00b181c3fb4f6bb692f5b139a8ed3f6a4f6f3
Christa Lococo <montecitocandleco@gmail.com>
2023-11-11 15:05:59 -0700

    updated main.jsx

--- React-Portfolio/src/main.jsx
+++ React-Portfolio/src/main.jsx
@@ -1,18 +1,16 @@
-import ReactDOM from 'react-dom/client';
-// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import ReactDOM from 'react-dom'; // Importing ReactDOM from 'react-dom'
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-// Bringing in the pages the router will use to conditionally show the appropriate views
 import App from './App';
 import ErrorPage from './pages/ErrorPage';
 import HomePage from './pages/HomePage';
 import ProfilePage from './pages/ProfilePage';
 import AboutPage from './pages/AboutPage';
 
-// Define the accessible routes, and which components respond to which URL
 const router = createBrowserRouter([
   {
     path: '/',
@@ -35,7 +33,6 @@ const router = createBrowserRouter([
   },
 ]);
 
-// Render the RouterProvider component
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
 );
