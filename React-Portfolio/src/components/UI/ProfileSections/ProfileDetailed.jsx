61db7973b27c7b6cdcdb72f9ed2d94baff97103d
Christa Lococo <montecitocandleco@gmail.com>
2023-11-11 11:34:42 -0700

    updated profileSections

--- React-Portfolio/src/components/UI/ProfileSections/ProfileDetailed.jsx
+++ React-Portfolio/src/components/UI/ProfileSections/ProfileDetailed.jsx
@@ -1,10 +1,14 @@
+import React from 'react';
 import CardHeader from '../Card/CardHeader';
 import CardBody from '../Card/CardBody';
 
 export default function ProfileDetailed({ user }) {
   return (
     <div className="card">
+      {/* CardHeader Component */}
       <CardHeader user={user} />
+
+      {/* CardBody Component */}
       <CardBody company={user.company} address={user.address} />
     </div>
   );
--- React-Portfolio/src/components/UI/ProfileSections/ProfileTeaser.jsx
+++ React-Portfolio/src/components/UI/ProfileSections/ProfileTeaser.jsx
@@ -1,14 +1,17 @@
+import React from 'react';
+
 export default function ProfileTeaser({ user: { username, name, email } }) {
-    const usernameConcat = `(${username})`;
-  
-    return (
-      <>
-        <div className="ms-2 me-auto">
-          <h2 className="fw-bold mb-1">{name}</h2>
-          <h5 className="mb-1">{usernameConcat}</h5>
-          <a href={`mailto:${email}`}>{email}</a>
-        </div>
-      </>
-    );
-  }
-  
+  const usernameConcat = `(${username})`;
+
+  return (
+    <div className="ms-2 me-auto">
+      <div>
+        <h2 className="card-title fw-bold mb-1">{name}</h2>
+        <h5 className="card-subtitle mb-1">{usernameConcat}</h5>
+      </div>
+      <a href={`mailto:${email}`} className="text-decoration-none">
+        {email}
+      </a>
+    </div>
+  );
+}
