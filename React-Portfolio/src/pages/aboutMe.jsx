7c96b3b9030fca63c08eb1379dddf4633d61db66
Christa Lococo <montecitocandleco@gmail.com>
2023-11-18 20:12:51 -0700

    buttons

--- React-Portfolio/src/pages/aboutMe.jsx
+++ React-Portfolio/src/pages/aboutMe.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import '../styles/styles.css'; 
-import AnimatedExample from '../components/Animated/AnimatedExample';
 
 const AboutMe = () => {
   return (
     <section id="about-me">
       <div className="about-me-content">
         <h2>About Me</h2>
-        <img src="path-to-your-image.jpg" alt="Your Avatar" className="avatar" />
+        <img src="/Users/View/Desktop/bootcamp/homework/ReactPortfolio/oldreactPortfolio/React-Portfolio/src/assets/image.jpg" alt="Avatar" className="avatar" />
         <p>
           Hi, I'm Christa Lococo, a passionate web developer with experience in creating
           awesome web applications. I love turning ideas into reality using code.
         </p>
-        <AnimatedExample /> {/* Include the suggested animated component */}
+
       </div>
     </section>
   );
--- React-Portfolio/src/pages/resume.jsx
+++ React-Portfolio/src/pages/resume.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/styles.css';
-import ExampleComponent from '../components/Animated/AnimatedExample';
+
 
 const skills = [
   { name: 'React', level: 'Advanced' },
